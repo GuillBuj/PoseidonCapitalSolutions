787c9117791c68fc3d2a30472a375430c7040a08
GuillBuj <guillaume.bujon@gmail.com>
2025-03-19 11:19:38 +0100

    bid entity + thymeleaf bidList->bid

--- src/main/java/com/poseidoncapitalsolutions/trading/model/Bid.java
+++ src/main/java/com/poseidoncapitalsolutions/trading/model/Bid.java
@@ -1,10 +1,46 @@
 package com.poseidoncapitalsolutions.trading.model;
 
+import java.sql.Timestamp;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "bid")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Bid {
-    // TODO: Map columns in data table BIDLIST with corresponding java fields
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String account;
+    private String type;
+    private Double bidQuantity;
+    private Double askQuantity;
+    private Double bid;
+    private Double ask;
+    private String benchmark;
+    private Timestamp bidListDate;
+    private String commentary;
+    private String security;
+    private String status;
+    private String trader;
+    private String book;
+    private String creationName;
+    private Timestamp creationDate;
+    private String revisionName;
+    private Timestamp revisionDate;
+    private String dealName;
+    private String dealType;
+    private String sourceListId;
+    private String side;
 }
