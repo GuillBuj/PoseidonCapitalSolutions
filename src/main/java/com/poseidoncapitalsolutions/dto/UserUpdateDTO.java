0066549d4ad8db7df049982f0d8941b9dd627799
GuillBuj <guillaume.bujon@gmail.com>
2025-03-19 10:41:16 +0100

    UserUpdate

---
+++ src/main/java/com/poseidoncapitalsolutions/dto/UserUpdateDTO.java
@@ -0,0 +1,29 @@
+package com.poseidoncapitalsolutions.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record UserUpdateDTO(
+    
+    @NotNull(message = "L'ID est obligatoire")
+    int id,
+
+    @NotBlank(message = "Username is mandatory")
+    String username,
+
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 8, message = "Le mot de passe doit contenir au moins 8 caractères")
+    @Pattern(
+        regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#^()])[A-Za-z\\d@$!%*?&#^()]{8,}$",
+        message = "Le mot de passe doit contenir au moins une majuscule, un chiffre et un symbole"
+    )
+    String rawPassword,
+
+    @NotBlank(message = "FullName is mandatory")
+    String fullname,
+
+    @NotBlank(message = "Role is mandatory")
+    String role
+) {}
--- src/main/java/com/poseidoncapitalsolutions/trading/controller/UserController.java
+++ src/main/java/com/poseidoncapitalsolutions/trading/controller/UserController.java
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.poseidoncapitalsolutions.dto.UserCreateDTO;
+import com.poseidoncapitalsolutions.dto.UserUpdateDTO;
 import com.poseidoncapitalsolutions.trading.model.User;
 import com.poseidoncapitalsolutions.trading.repository.UserRepository;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-
 
 @Controller
 @Slf4j
@@ -27,8 +27,7 @@ public class UserController {
     private UserRepository userRepository;
 
     @RequestMapping("/user/list")
-    public String home(Model model)
-    {
+    public String home(Model model) {
         model.addAttribute("users", userRepository.findAll());
         return "user/list";
     }
@@ -36,7 +35,7 @@ public class UserController {
     @GetMapping("/user/add")
     public String addUser(Model model) {
         model.addAttribute("userDTO", new UserCreateDTO("", "", "", "USER"));
-        
+
         return "user/add";
     }
 
@@ -56,37 +55,47 @@ public class UserController {
         newUser.setPassword(encoder.encode(userDTO.rawPassword()));
 
         userRepository.save(newUser);
-        model.addAttribute("users", userRepository.findAll());
 
         return "redirect:/user/list";
     }
 
     @GetMapping("/user/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-        user.setPassword("");
-        model.addAttribute("user", user);
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+
+        model.addAttribute("userDTO",
+                new UserUpdateDTO(user.getId(), user.getUsername(), "", user.getFullname(), user.getRole()));
         return "user/update";
     }
 
-    @PostMapping("/user/update/{id}")
-    public String updateUser(@PathVariable("id") Integer id, @Valid User user,
-                             BindingResult result, Model model) {
+    @PostMapping("/user/update")
+    public String updateUser(@Valid @ModelAttribute("userDTO") UserUpdateDTO userDTO, BindingResult result, Model model) {
         if (result.hasErrors()) {
+            model.addAttribute("userDTO", userDTO);
             return "user/update";
         }
 
+        User user = userRepository.findById(userDTO.id())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        user.setUsername(userDTO.username());
+        user.setFullname(userDTO.fullname());
+        user.setRole(userDTO.role());
+
+        // todo: voir si ça convient
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        user.setPassword(encoder.encode(user.getPassword()));
-        user.setId(id);
+        user.setPassword(encoder.encode(userDTO.rawPassword()));
+
         userRepository.save(user);
-        model.addAttribute("users", userRepository.findAll());
+
         return "redirect:/user/list";
     }
 
     @GetMapping("/user/delete/{id}")
     public String deleteUser(@PathVariable("id") Integer id, Model model) {
-        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
         userRepository.delete(user);
         model.addAttribute("users", userRepository.findAll());
         return "redirect:/user/list";
